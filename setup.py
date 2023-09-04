6074fbd0cf3301c66d469b3f18081fab466d1110
Graham Hukill <ghukill@gmail.com>
2023-09-03 22:19:17 -0400

    scaffolding library configs and fixtures

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup  # type: ignore[import]
+
+setup(
+    name="setter",
+    version="0.1.0",
+    description="TODO",
+    author="Graham Hukill",
+    author_email="ghukill@gmail.com",
+    url="https://github.com/ghukill/setter",
+    packages=find_packages(),
+    install_requires=[],
+    extras_require={
+        "xml": [
+            "lxml>=4.9.0",
+        ],
+        "dataframe": [
+            "pandas>=2.1.0",
+            "duckdb>=0.8.1",
+        ],
+    },
+)
