2c8bd8b01e88a34475eee755448b83fae079f4ab
Graham Hukill <ghukill@gmail.com>
2023-09-06 16:11:02 -0400

    setting up build tools

--- setup.py
+++ setup.py
@@ -9,18 +9,18 @@ setup(
     url="https://github.com/ghukill/steamboat",
     packages=find_packages(),
     install_requires=[
-        "attrs==23.1.0",
-        "mypy==1.5.1",
-        "networkx==3.1",
-        "networkx-stubs==0.0.1",
+        "attrs>=23.1.0",
+        "mypy>=1.5.1",
+        "networkx>=3.1",
+        "networkx-stubs>=0.0.1",
     ],
     extras_require={
         "xml": [
             "lxml>=4.9.0",
         ],
         "dataframe": [
-            "duckdb==0.8.1",
-            "pandas==2.0.3",
+            "duckdb>=0.8.1",
+            "pandas>=2.0.3",
         ],
     },
 )
